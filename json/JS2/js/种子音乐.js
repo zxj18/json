2e272f00edc07f34c38952172ad28314f33e8fa3
jing <12578931+io9@user.noreply.gitee.com>
2023-06-29 13:16:11 +0000

    update json/JS2/js/种子音乐.js.
    Signed-off-by: jing <12578931+io9@user.noreply.gitee.com>

--- json/JS2/js/种子音乐.js
+++ json/JS2/js/种子音乐.js
@@ -1,6 +1,6 @@
 var rule = {
     title:'种子音乐',
-    host:'https://www.zz123.com',
+    host:'https://www.zz123.com/',
     homeUrl:'/list/mszm.htm?page=1',
     url:'/list/fyclass.htm?page=fypage',
     searchable:2,
@@ -12,7 +12,7 @@ var rule = {
     class_parse: '.taglist li;.tagitem&&data-name;.tagitem&&data-id',
     play_parse:true,
     // lazy:"js:input=input.replace(/play\\/(\\w+)\\.htm/,'ajax/?act=songinfo&id=$1&lang=');log(input);let mp3 = JSON.parse(request(input)).data.mp3;input={jx:0,url:mp3,parse:1,header:JSON.stringify({Referer:'https://www.zz123.com/'})}",
-    lazy:"js:input=input.replace(/play\\/(\\w+)\\.htm/,'ajax/?act=songinfo&id=$1&lang=');log(input);let mp3=JSON.parse(request(input)).data.mp3;input={jx:0,url:'https://zz123.com'+mp3,parse:1,header:JSON.stringify({Referer:'https://zz123.com/'})}",
+    lazy:"js:input=input.replace(/play\\/(\\w+)\\.htm/,'ajax/?act=songinfo&id=$1&lang=').replace('www.','');log(input);let mp3=JSON.parse(request(input)).data.mp3;input={jx:0,url:'https://zz123.com/'+mp3,parse:1,header:JSON.stringify({Referer:'https://www.zz123.com/'})}",
     limit:6,
     图片来源:'@Referer=https://www.zz123.com/',
     推荐:"*",
