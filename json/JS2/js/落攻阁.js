e5a1fea1482aa6ea329f8ed4c24a388a6c5a41a3
jing <12578931+io9@user.noreply.gitee.com>
2023-06-30 01:52:03 +0000

    update json/JS2/js/落攻阁.js.
    Signed-off-by: jing <12578931+io9@user.noreply.gitee.com>

--- json/JS2/js/落攻阁.js
+++ json/JS2/js/落攻阁.js
@@ -1,7 +1,7 @@
 muban.mxpro.二级.desc = '.module-info-item:eq(4)&&Text;;;.module-info-item-content:eq(1)&&Text;.module-info-item-content:eq(0)&&Text';
 muban.mxpro.二级.tabs = '#y-playList&&.tab-item';
 var rule = {
-    title:'落攻阁影视',
+    title:'落攻阁',
     模板:'mxpro',
     host:'https://www.lggys.cn',
     // url:'/index.php/vod/show/id/fyclass/page/fypage.html',
