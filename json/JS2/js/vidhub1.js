e3a66b0f76688ab72cbd5f2650e5e638e9e16dab
jing <12578931+io9@user.noreply.gitee.com>
2023-07-20 13:16:40 +0000

    update json/JS2/js/vidhub1.js.
    Signed-off-by: jing <12578931+io9@user.noreply.gitee.com>

--- json/JS2/js/vidhub1.js
+++ json/JS2/js/vidhub1.js
@@ -3,7 +3,7 @@ var rule = {
 	title:'vidhub',
 	模板:'mxpro',
 	host:'https://vidhub.cc/',
-	url:'/vodshow/fyfilter.html',
+	url:'/show/fyfilter.html',
     searchable: 1,
     quickSearch: 1,
 	filterable:1,//是否启用分类筛选,
