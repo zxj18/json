03215aea72bc6794c66dda6e04550d95308540d2
jing <12578931+io9@user.noreply.gitee.com>
2023-07-24 03:16:39 +0000

    update json/JS2/js/vidhub.js.
    Signed-off-by: jing <12578931+io9@user.noreply.gitee.com>

--- json/JS2/js/vidhub.js
+++ json/JS2/js/vidhub.js
@@ -1,8 +1,10 @@
 muban.mxone5.二级.desc = ';;.video-info-items:eq(2);.video-info-items:eq(1);.video-info-items:eq(0)';
 var rule = Object.assign(muban.mxone5,{
+    //title:'Vid影视',
     title:'Vid影视',
     host:'https://vidhub.cc',
-    url:'/vodshow/fyclass--------fypage---.html',
+    //url:'/vodshow/fyclass--------fypage---.html',
+    url:'/vodshow/fyclassfyfilte.html',
     searchUrl:'/vodsearch/**----------fypage---.html',
     cate_exclude:'网址+',
     tab_exclude:'主选一',
