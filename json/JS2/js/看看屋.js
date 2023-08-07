52faf8c579a99429764025fb7a7fa660fa66eb33
jing <12578931+io9@user.noreply.gitee.com>
2023-08-07 11:07:20 +0000

    update json/JS2/js/看看屋.js.
    Signed-off-by: jing <12578931+io9@user.noreply.gitee.com>

--- json/JS2/js/看看屋.js
+++ json/JS2/js/看看屋.js
@@ -4,7 +4,7 @@ var rule = {
 	title:'看看屋',
 	模板:'mxpro',
 	host:'https://www.kkw361.com',
-	url:'/vodshow/{cateId}-{area}-{by}-{class}-{lang}-{letter}---fypage---{year}.html',
+	url:'/vodshow/{cateId}-{area}-{by}-{class}-{lang}-{letter}------{year}.html',
 	//url:'/vodshow/fyclassfilter.html',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
